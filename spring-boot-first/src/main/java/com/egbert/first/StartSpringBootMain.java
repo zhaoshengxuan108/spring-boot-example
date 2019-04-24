1831c2fe79eafdc6d6b4cee4c035b09f1769065c
赵亮 <zhao.liang@allscore.com>
2019-04-24 15:04:07 +0800

    添加第一个子项目
    添加第一个子项目spring-boot-first

---
+++ spring-boot-first/src/main/java/com/egbert/first/StartSpringBootMain.java
@@ -0,0 +1,16 @@
+package com.egbert.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartSpringBootMain {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(StartSpringBootMain.class, args);
+    }
+
+}
+
+
+
